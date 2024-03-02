7ba79ec06102af39216a78a6404b564cf18caae3
shaik <irfans086836@gmail.com>
2024-03-02 22:50:21 +0530

    initial commit

---
+++ src/main/java/com/mtls/mtls/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.mtls.mtls;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MtlsApplication.class);
+	}
+
+}
